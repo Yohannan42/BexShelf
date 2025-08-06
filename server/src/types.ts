89dee8b3db596751797952fe54564537976f8240
Yohannan42 <yohannanwol@gmail.com>
2025-08-06 09:46:25 -0400

    Fix Express Request type definition for authentication

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,3 +1,17 @@
+import { Request } from 'express';
+
+// Extend Express Request to include user property
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        userId: string;
+        email: string;
+      };
+    }
+  }
+}
+
 export interface WritingProject {
   id: string;
   userId: string;
